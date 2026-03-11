290eba8021c10a8639516e9ea3ad372a32f77ff8
SachinKumar0427 <sk4718@gmail.com>
2026-03-11 17:15:05 +0530

    delete Email.js

--- NarawearBackend/utils/Email.js
+++
@@ -1,9 +0,0 @@
-// const nodemailer = require("nodemailer")
-// exports.mailer = nodemailer.createTransport({
-//     service: "gmail", 
-//     auth: {
-//         user: "bhagath0427@gmail.com",
-//         pass: "yuqz siux ldpw jpxl"
-//     }
-// });
-
